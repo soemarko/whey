102c9c9dad1813c6a40c3fab0296d81c1bf28d9a
soemarko <soemarko@users.noreply.github.com>
2021-09-22 02:37:35 +0700

    restructure the paths

---
+++ Sketch/whey-receiver/espnow.ino
@@ -0,0 +1,91 @@
+#include <esp_now.h>
+#include <WiFi.h>
+
+int32_t tareOffset = 0;
+float calFactor = 1.0;
+bool autoCalibrate  = false;
+
+typedef struct struct_message {
+    int32_t value;
+} struct_message;
+
+struct_message myData;
+
+// callback function that will be executed when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&myData, incomingData, sizeof(myData));
+  weight = raw_to_unit(myData.value);
+  if (autoCalibrate) autoCalibration();
+}
+
+void espnow_setup() {
+	// Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+  
+  Serial.print("mac: ");
+  Serial.println(WiFi.macAddress());
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_register_recv_cb(OnDataRecv);
+}
+
+void espnow_loop() {
+	
+}
+
+// ***************** WEIGHT
+
+void tare() {
+	tareOffset = myData.value;
+}
+
+void autoCalibration() {
+	if(weight >= -0.001 || weight <= 0.001) {
+		// auto calibration complete
+		autoCalibrate  = false;
+		return;
+	}
+	
+	if (weight > 0.0) {
+		//increase calfactor
+		calFactor = calFactor + 1;
+	}
+	else {
+		//decrease calfactor
+		calFactor = calFactor - 1;
+	}
+}
+
+void calibrate(TFT_eSPI tft) {
+	calFactor = 1.0;
+	tare();
+
+	Serial.println("Calibrating");
+	
+  tft.fillScreen(TFT_BLACK);
+	tft.setTextFont(1);
+	tft.setTextSize(2);
+	tft.setCursor(0, 13);
+	tft.print("   Calibrating!\n\nPlace 100g object\n");
+
+	for(int i=9; i>=0; i--) {
+		tft.print(".");
+		Serial.println(i);
+		delay(1000);
+	}
+
+	calFactor = (myData.value - tareOffset) / 100.0;
+	Serial.println("done");
+	tft.print("\n\n        Done.");
+	delay(1000);
+  tft.fillScreen(TFT_BLACK);
+}
+
+double raw_to_unit(long raw) {
+	return (double)(raw - tareOffset)/(double)calFactor;
+}
---
+++ Sketch/whey-receiver/whey-receiver.ino
@@ -0,0 +1,42 @@
+
+#include <TFT_eSPI.h>
+#include <SPI.h>
+
+// ***************** TIMER
+
+bool timeStart = false;
+volatile int seconds = -4;
+hw_timer_t * timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
+void IRAM_ATTR onTimer() {
+	portENTER_CRITICAL_ISR(&timerMux);
+	seconds += 1;
+	portEXIT_CRITICAL_ISR(&timerMux);
+}
+
+float weight = 0.0;
+
+void setup() {
+  // Initialize Serial Monitor
+  Serial.begin(115200);
+  
+  espnow_setup();
+
+  screen_init();
+  button_init();
+
+	timer = timerBegin(0, 80, true);
+	timerAttachInterrupt(timer, &onTimer, true);
+	timerAlarmWrite(timer, 1000000, true);
+}
+
+void loop() {
+  espnow_loop();
+  
+	button_loop();
+	screen_loop();
+
+	if(timeStart) timerAlarmEnable(timer);
+	else timerAlarmDisable(timer);
+}
